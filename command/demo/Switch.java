48f9867f9344abd36c190a1f2bad0d9d025d6720
Mai Thanh Hảo <109146707+HaoHao1000@users.noreply.github.com>
2023-01-08 01:45:54 +0700

    Add files via upload

---
+++ command/demo/Switch.java
@@ -0,0 +1,15 @@
+package command.demo;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Switch {
+    private List<Command> history = new ArrayList<>();
+
+    public void storeAndExecute(Command command) {
+        history.add(command);
+        command.execute();
+    }
+}
+
+
