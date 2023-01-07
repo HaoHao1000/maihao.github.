72395f29d160dfc887aa6491eefed4fcf6a965f1
Mai Thanh Hảo <109146707+HaoHao1000@users.noreply.github.com>
2023-01-08 01:37:06 +0700

    Add files via upload

---
+++ homework10/collections/mymap/MyMap.java
@@ -0,0 +1,10 @@
+package com.oop.collections.mymap;
+
+public interface MyMap {
+    public int size();
+    public void put(Object key, Object value);
+    void remove(Object key);
+    boolean contains(Object key);
+
+
+}
---
+++ homework10/collections/phonebook/PhoneBookMap.java
@@ -0,0 +1,57 @@
+package com.oop.collections.phonebook;
+
+import java.util.HashMap;
+
+public class PhoneBookMap implements PhoneBook{
+    HashMap<String, Student> phoneBook;
+
+    public PhoneBookMap(){
+        phoneBook = new HashMap<>();
+    }
+
+    @Override
+    public void addPerson(Student p) {
+        if (!phoneBook.containsKey(p.phone)){
+            phoneBook.put(p.phone, p);
+        }
+    }
+
+    @Override
+    public Student searchByName(String name) {
+        for (Student student : phoneBook.values()) {
+            if (student.name.equals(name)){
+                return student;
+            }
+        }
+        return null;
+    }
+
+    @Override
+    public Student searchByLastName(String lastname) {
+        for (Student student : phoneBook.values()) {
+            if (student.name.equals(lastname)){
+                return student;
+            }
+        }
+        return null;
+    }
+
+    @Override
+    public Student searchByNumber(String phone) {
+        for (Student student : phoneBook.values()) {
+            if (student.name.equals(phone)){
+                return student;
+            }
+        }
+        return null;
+    }
+
+    @Override
+    public void deleteByNumber(String phone) {
+        Student student = searchByNumber(phone);
+        if (student != null){
+            phoneBook.remove(student);
+        }
+
+    }
+}
