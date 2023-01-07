1ec4881d18aa4ffaac160be5ab76ab5880fd40f7
Mai Thanh Hảo <109146707+HaoHao1000@users.noreply.github.com>
2023-01-08 01:47:59 +0700

    Add files via upload

---
+++ bridge/demo/OperatingSystem.java
@@ -0,0 +1,8 @@
+package bridge.demo;
+
+public interface OperatingSystem {
+    void startUp();
+    void loadUrl(String url);
+}
+
+
---
+++ bridge/demo/WindowsOS.java
@@ -0,0 +1,15 @@
+package bridge.demo;
+
+public class WindowsOS implements OperatingSystem {
+    @Override
+    public void startUp() {
+        System.out.println("WindowsOS start up");
+    }
+
+    @Override
+    public void loadUrl(String url) {
+        System.out.println("Load " + url + " from Edge");
+    }
+}
+
+
